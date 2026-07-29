eab294f465dc777a41981308950b5e07b9684b7a
Thomas <thomas@nytveit.net>
2026-07-29 23:29:20 +0200

    Update service worker to version 2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "octacore-sykemeldingskalkulator-v1";
+const CACHE_NAME =
+    "octacore-sykemeldingskalkulator-v2";
 
 const APP_ASSETS = [
     "./",
@@ -18,75 +19,123 @@ const APP_ASSETS = [
     "./icon-512.png"
 ];
 
-// Installer service worker
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(APP_ASSETS);
-        })
-    );
-
-    self.skipWaiting();
-});
-
-// Fjern gamle cacher
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(cacheName => cacheName !== CACHE_NAME)
-                    .map(cacheName => caches.delete(cacheName))
-            );
-        })
-    );
-
-    self.clients.claim();
-});
-
-// Håndter forespørsler
-self.addEventListener("fetch", event => {
-
-    if (event.request.method !== "GET") {
-        return;
-    }
-
-    event.respondWith(
-
-        caches.match(event.request).then(cachedResponse => {
-
-            if (cachedResponse) {
-                return cachedResponse;
-            }
-
-            return fetch(event.request)
-                .then(networkResponse => {
-
-                    if (
-                        !networkResponse ||
-                        networkResponse.status !== 200 ||
-                        networkResponse.type === "opaque"
-                    ) {
-                        return networkResponse;
+self.addEventListener(
+    "install",
+    event => {
+        event.waitUntil(
+            caches
+                .open(
+                    CACHE_NAME
+                )
+                .then(
+                    cache => {
+                        return cache.addAll(
+                            APP_ASSETS
+                        );
                     }
+                )
+        );
 
-                    const responseToCache = networkResponse.clone();
+        self.skipWaiting();
+    }
+);
 
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, responseToCache);
-                    });
+self.addEventListener(
+    "activate",
+    event => {
+        event.waitUntil(
+            caches
+                .keys()
+                .then(
+                    cacheNames => {
+                        return Promise.all(
+                            cacheNames
+                                .filter(
+                                    cacheName =>
+                                        cacheName !==
+                                        CACHE_NAME
+                                )
+                                .map(
+                                    cacheName =>
+                                        caches.delete(
+                                            cacheName
+                                        )
+                                )
+                        );
+                    }
+                )
+        );
 
-                    return networkResponse;
+        self.clients.claim();
+    }
+);
 
-                })
-                .catch(() => {
+self.addEventListener(
+    "fetch",
+    event => {
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+            return;
+        }
 
-                    return caches.match("./index.html");
+        event.respondWith(
+            caches
+                .match(
+                    event.request
+                )
+                .then(
+                    cachedResponse => {
+                        const networkFetch =
+                            fetch(
+                                event.request
+                            )
+                                .then(
+                                    networkResponse => {
+                                        if (
+                                            !networkResponse ||
+                                            networkResponse.status !==
+                                                200 ||
+                                            networkResponse.type ===
+                                                "opaque"
+                                        ) {
+                                            return networkResponse;
+                                        }
 
-                });
+                                        const responseToCache =
+                                            networkResponse.clone();
 
-        })
+                                        caches
+                                            .open(
+                                                CACHE_NAME
+                                            )
+                                            .then(
+                                                cache => {
+                                                    cache.put(
+                                                        event.request,
+                                                        responseToCache
+                                                    );
+                                                }
+                                            );
 
-    );
+                                        return networkResponse;
+                                    }
+                                )
+                                .catch(
+                                    () =>
+                                        cachedResponse ||
+                                        caches.match(
+                                            "./index.html"
+                                        )
+                                );
 
-});
+                        return (
+                            cachedResponse ||
+                            networkFetch
+                        );
+                    }
+                )
+        );
+    }
+);
